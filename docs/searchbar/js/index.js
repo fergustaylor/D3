47497dd7d7ec0a212f416b7ef0c6fefea426f5d5
Fergus Taylor <fergustay@gmail.com>
2018-01-18 11:35:58 +0000

    sdfghjk

--- docs/searchbar/js/index.js
+++ docs/searchbar/js/index.js
@@ -293,3 +293,16 @@ jQuery('.hoveroverlap').show();
           jQuery('#div'+$(this).attr('target')).show();
                 });
 };
+
+jQuery('#showall').click(function(){
+  jQuery('.targetDiv').show();
+        });
+
+jQuery('#hideall').click(function(){
+  jQuery('.targetDiv').hide();
+        });
+
+jQuery('.showSingle').click(function(){
+  jQuery('.targetDiv').hide();
+  jQuery('#div'+$(this).attr('target')).show();
+        });
