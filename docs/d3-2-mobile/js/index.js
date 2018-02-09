da3c037ee2c5870247492a8ea23c0725a0233a43
Fergus Taylor <fergustay@gmail.com>
2018-02-09 23:58:25 +0000

    xcvbn

--- docs/d3-2-mobile/js/index.js
+++ docs/d3-2-mobile/js/index.js
@@ -25,3 +25,40 @@ function toggleCheckbox(element)
      };
    }
  }
+
+ jQuery(document).ready(function ($) {
+     if (isTouchSupported || isTouchSupportedIE10) {
+         // this is a mobile device
+         if (window.orientation == 0 || window.orientation == 180) {
+             // run init for portrait orientation
+         } else if (window.orientation == 90 || window.orientation == -90) {
+             // run init for landscape orientation
+         }
+         // detect orientation changes
+         window.addEventListener("orientationchange", function () {
+             if (window.orientation === 0 || window.orientation === 180) {
+                 // changed to portrait
+                 var div = document.createElement("div");
+                                  div.style.width = "100vw";
+                                  div.style.height = "100vh";
+                                  div.style.background = "white";
+                                  div.style.color = "black";
+                                  div.style.opacity = "0.9";
+                                  div.style.zindex = "10000000";
+
+                 var note = document.createElement("p")
+                 note.innerHTML = "Hello, this has 100-ish drugs around the diameter. Best to turn landscape, and reload for this one. <br>\<br>\Fergus";
+
+                 note.style.font = '300 11px "Helvetica Neue", Helvetica, Arial, sans-serif"';
+                 note.style.textAlign = "center";
+
+                 div.appendChild(note);
+
+                 document.body.appendChild(div);
+
+             } else if (window.orientation == 90 || window.orientation == -90) {
+                 // changed to landscape
+             }
+         }, false);
+     }
+ });
