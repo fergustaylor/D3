24bc9216ded082b8a4669f7ef6c0c7ef3eca3c4e
Fergus Taylor <fergustay@gmail.com>
2018-02-12 20:40:21 +0000

    dscfghjkl

--- docs/searchbardev/js/index.js
+++ docs/searchbardev/js/index.js
@@ -36,12 +36,10 @@ input.placeholder = "Loading the BNF...";
 request.open('GET', 'https://fergustaylor.github.io/D3/dev/flare2electricboogaloo.json', true);
 request.send();
 
-var myInput =  document.getElementById('search');
-var myDataList = document.getElementById('json-datalist')
 var myOptions = document.getElementById('json-datalist').childNodes;
 
 function listOptionSelected() {
-    var myValue = myInput.value;
+    var myValue = input.value;
     for (var i = 0; i < myOptions.length; i++) {
         if (myOptions[i].value === myValue) {
             console.log('Option Selected: ' + myValue);
@@ -49,18 +47,7 @@ function listOptionSelected() {
     }
 }
 
-function typedOptionSelected() {
-    var myValue = myInput.value;
-    if (myValue !== '') {
-        console.log('Option Selected: ' + myInput.value);
-    }
-}
-
-myInput.addEventListener('input', listOptionSelected, false);
-myInput.addEventListener('blur', typedOptionSelected, false);
-
-
-//
+input.addEventListener('input', listOptionSelected, false);
 
 jQuery(function(){
 jQuery('#close').click(function(){
