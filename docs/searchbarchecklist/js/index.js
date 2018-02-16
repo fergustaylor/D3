c19ce20d6f773739783e242ce722d0dd2d84c8bf
Fergus Taylor <fergustay@gmail.com>
2018-02-16 18:27:24 +0000

    dfghjkl

---
+++ docs/searchbarchecklist/js/index.js
@@ -0,0 +1,29 @@
+var checklistcontainer = document.getElementById('checklist');
+// Create a new XMLHttpRequest.
+var request = new XMLHttpRequest();
+// Handle state changes for the request.
+request.onreadystatechange = function(response) {
+  if (request.readyState === 4) {
+    if (request.status === 200) {
+      // Parse the JSON
+      var jsonOptions = JSON.parse(request.responseText);
+      // Loop over the JSON array.
+     jsonOptions.forEach(function(item) {
+        // Create a new <option> element.
+        var checklists = document.createElement('p');
+        // Set the value using the item in the JSON array.
+        var checklists5 = document.createTextNode(item.title);
+        checklists.appendChild(checklists5);
+        //add a break
+        var breaks = document.createElement('br');
+        // Add the <p> element to the <div>.
+        checklistcontainer.appendChild(checklists);
+        // Add the <br> element to the <div>.
+        checklistcontainer.appendChild(breaks);
+      });
+    }
+  }
+};
+// Set up and make the request.
+request.open('GET', 'https://fergustaylor.github.io/D3/dev/flare2electricboogaloo.json', true);
+request.send();
