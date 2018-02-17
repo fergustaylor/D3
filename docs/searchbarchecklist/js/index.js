3324cf844696d15fd72691821d6cbb02caf12e5d
Fergus Taylor <fergustay@gmail.com>
2018-02-17 20:45:41 +0000

    fghjkl

--- docs/searchbarchecklist/js/index.js
+++ docs/searchbarchecklist/js/index.js
@@ -96,3 +96,12 @@ function showsearched(string) {
     searched2[i].style.display = 'block';
   }
 }
+
+function buildgraph() {
+  var selected = $('div#checklist>span>input:checked' );
+  var druginputarray = [];
+  //create array
+  for (i = 0; i < selected.length; i++) {
+    druginputarray.push(selected[i].value);
+  }
+}
