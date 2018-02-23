931623e2bef8c991146dcffa9eb6533eba56c70f
Fergus Taylor <fergustay@gmail.com>
2018-02-23 21:54:53 +0000

    dfghjkl;

--- docs/d3-dynamic/js/textscript.js
+++ docs/d3-dynamic/js/textscript.js
@@ -65,10 +65,11 @@ d3.json("https://fergustaylor.github.io/D3/dev/flare.json", function(classes) {
       .enter().append("svg:path")
       //add severity to each path by class
       //.attr("class", function(d) { return "link source-" + d.source.key + " target-" + d.target.key + " " + d.source.Severity[d.source.importstitle.indexOf(d.target.key)];})
-      .attr("class", function(d) { return "link source-" + d.source.key + " target-" + d.target.key + " " + d.source.Severity[d.source.importstitle.indexOf(d.target.title)];})
+      //.attr("class", function(d) { return "link source-" + d.source.key + " target-" + d.target.key + " " + d.source.Severity[d.source.importstitle.indexOf(d.target.title)];})
+      .attr("class", function(d) { return "link source-" + d.source.key + " target-" + d.target.key;})
       .attr("value", function(d) {return d.source.title})
-      .attr("severity", function(d) {return d.source.Severity[d.source.importstitle.indexOf(d.target.key)];})
-      //.attr("interaction", function(d) {return d.inter})
+      .attr("severity", function(d) {return d.source.Severity[d.source.importstitle.indexOf(d.target.title)];})
+      .attr("interaction", function(d) {return d.source["Interactions Info"][d.source.importstitle.indexOf(d.target.key)];})
       .attr("d", function(d, i) { return line(splines[i]); });
 
   svg.selectAll("g.node")
