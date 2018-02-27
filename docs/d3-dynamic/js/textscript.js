06c23bcb4c43e048c3e84bc20cb997cc2e2cfa62
Fergus Taylor <fergustay@gmail.com>
2018-02-27 17:17:54 +0000

    cfghjk

--- docs/d3-dynamic/js/textscript.js
+++ docs/d3-dynamic/js/textscript.js
@@ -41,7 +41,7 @@ svg.append("svg:path")
     .attr("d", d3.svg.arc().outerRadius(ry - 120).innerRadius(0).startAngle(0).endAngle(2 * Math.PI))
     .on("mousedown", mousedown);
 
-d3.json("https://fergustaylor.github.io/D3/dev/attemptjson.json", function(classes) {
+d3.json("https://fergustaylor.github.io/D3/dev/flare.json", function(classes) {
 
   //Update Timestamp
   document.getElementById('disclaimer').innerHTML = "This data was last updated " + classes[0].Stamp;
