a6984ce2f1d252d28268c0a587e977e71875c8a4
Fergus Taylor <fergustay@gmail.com>
2018-03-05 19:10:57 +0000

    cfghj

--- docs/bnf-interactionsdev/js/graph.js
+++ docs/bnf-interactionsdev/js/graph.js
@@ -60,4 +60,61 @@ function listit (druginputarray) {
 //Clear previous HTML
 document.body.innerHTML = '';
 document.head.innerHTML = '';
+
+//insert.js
+var insertjs = document.createElement("script");
+insertjs.setAttribute("src", "js/insert.js");
+document.body.appendChild(insertjs);
+
+var node40 = document.createElement("div");
+node40.setAttribute("class", "notice");
+document.body.appendChild(node40);
+
+var node41 = document.createElement("p");
+var node42 = document.createTextNode("This graph is based on ");
+node41.appendChild(node42);
+
+var node43 = document.createElement("a");
+node43.setAttribute("href", "https://bnf.nice.org.uk/interaction/");
+var node44 = document.createTextNode("the BNF.");
+node43.appendChild(node44);
+
+node41.appendChild(node43);
+node40.appendChild(node41);
+
+var node45 = document.createElement("p");
+node45.setAttribute("id", "disclaimer")
+var node46 = document.createTextNode("This data was last updated X.")
+node45.appendChild(node46);
+node40.appendChild(node45);
+
+//new css
+var newcss = document.createElement("link");
+newcss.setAttribute("rel", "stylesheet");
+newcss.setAttribute("href", "css/liststyle.css");
+document.head.appendChild(newcss);
+
+d3.json("https://fergustaylor.github.io/D3/dev/flare.json", function(classes) {
+
+  //Update Timestamp
+  document.getElementById('disclaimer').innerHTML = "This data was last updated " + classes[0].Stamp;
+
+  var classes2 = []
+  //add key to classes array
+  for (ii = 0; ii < classes.length; ii++) {
+    classes[ii].key = classes[ii].name.substring(i = classes[ii].name.lastIndexOf(".") + 1)
+  }
+  //filter classes array
+  for (i = 0; i < druginputarray.length; i++) {
+    for (ii = 0; ii < classes.length; ii++) {
+      if (classes[ii].key == druginputarray[i]) {
+        classes2.push(classes[ii])
+      }
+    }
+  };
+
+  var nodes = cluster.nodes(packages.root(classes2)),
+      links = packages.imports(nodes),
+      splines = bundle(links);
+});
 }
