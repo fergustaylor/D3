99162fca40e36cff9f058f91a52c9afa5baf7d70
Fergus Taylor <fergustay@gmail.com>
2018-05-25 13:01:17 +0100

    sadfghjk

--- docs/d3-2multiply/d3-2multiply.js
+++
@@ -1,8 +0,0 @@
-link = document.getElementsByClassName("link");
-for (i = 0; i < link.length; i++) { 
-   link[i].style.display = "none";
-}
-linksevere = document.getElementsByClassName("link Severe");
-for (i = 0; i < linksevere.length; i++) { 
-   linksevere[i].style.display = "block";
-}
