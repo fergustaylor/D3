bc9e30592717a360394ee3f711a392c8aed36437
Fergus Taylor <fergustay@gmail.com>
2018-05-25 12:46:43 +0100

    added css mix-mode-multiply

---
+++ docs/d3-2multiply/d3-2multiply.js
@@ -0,0 +1,8 @@
+link = document.getElementsByClassName("link");
+for (i = 0; i < link.length; i++) { 
+   link[i].style.display = "none";
+}
+linksevere = document.getElementsByClassName("link Severe");
+for (i = 0; i < linksevere.length; i++) { 
+   linksevere[i].style.display = "block";
+}
